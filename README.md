f71b637f0059151fd749e88e8cd9fd6204cfe012
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2022-04-13 14:26:31 -0400

    Updated app to use DATABASE_URL

--- README.md
+++ README.md
@@ -2,14 +2,4 @@
 
 This repo has a "Hello World" Go application that uses the [pq driver](https://godoc.org/github.com/lib/pq) to talk to [CockroachDB](https://www.cockroachlabs.com/docs/stable/).
 
-To run the code:
-
-1. Start a [demo CockroachDB cluster](https://www.cockroachlabs.com/docs/stable/cockroach-demo.html) from the command line: `cockroach demo --empty`
-
-1. Create a `bank` database and a user and password as described in [Build a Go app with CockroachDB](https://www.cockroachlabs.com/docs/stable/build-a-go-app-with-cockroachdb-pq.html).
-
-1. From the SQL client: `GRANT ALL ON DATABASE bank TO user`
-
-1. Update the connection string passed to `sql.Open()` with the correct connection values.
-
-1. In your terminal, from this directory: `go mod init basic-sample && go run main.go`
+For instructions on running the code in this repo, see [Build a Go app with CockroachDB](https://www.cockroachlabs.com/docs/stable/build-a-go-app-with-cockroachdb-pq.html).
--- main.go
+++ main.go
@@ -1,88 +1,89 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"fmt"
-	"github.com/cockroachdb/cockroach-go/crdb"
-	_ "github.com/lib/pq"
-	"log"
+        "context"
+        "database/sql"
+        "fmt"
+        "log"
+        "os"
+
+        "github.com/cockroachdb/cockroach-go/crdb"
+        _ "github.com/lib/pq"
 )
 
 func printBalances(db *sql.DB) {
-	// Print out the balances.
-	rows, err := db.Query("SELECT id, balance FROM accounts")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	fmt.Println("Balances:")
-	for rows.Next() {
-		var id, balance int
-		if err := rows.Scan(&id, &balance); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%d %d\n", id, balance)
-	}
+        // Print out the balances.
+        rows, err := db.Query("SELECT id, balance FROM accounts")
+        if err != nil {
+                log.Fatal(err)
+        }
+        defer rows.Close()
+        fmt.Println("Balances:")
+        for rows.Next() {
+                var id, balance int
+                if err := rows.Scan(&id, &balance); err != nil {
+                        log.Fatal(err)
+                }
+                fmt.Printf("%d %d\n", id, balance)
+        }
 }
 
 func transferFunds(tx *sql.Tx, from int, to int, amount int) error {
-	// Read the balance.
-	var fromBalance int
-	if err := tx.QueryRow(
-		"SELECT balance FROM accounts WHERE id = $1", from).Scan(&fromBalance); err != nil {
-		return err
-	}
+        // Read the balance.
+        var fromBalance int
+        if err := tx.QueryRow(
+                "SELECT balance FROM accounts WHERE id = $1", from).Scan(&fromBalance); err != nil {
+                return err
+        }
 
-	if fromBalance < amount {
-		return fmt.Errorf("insufficient funds")
-	}
+        if fromBalance < amount {
+                return fmt.Errorf("insufficient funds")
+        }
 
-	// Perform the transfer.
-	if _, err := tx.Exec(
-		"UPDATE accounts SET balance = balance - $1 WHERE id = $2", amount, from); err != nil {
-		return err
-	}
-	if _, err := tx.Exec(
-		"UPDATE accounts SET balance = balance + $1 WHERE id = $2", amount, to); err != nil {
-		return err
-	}
-	return nil
+        // Perform the transfer.
+        if _, err := tx.Exec(
+                "UPDATE accounts SET balance = balance - $1 WHERE id = $2", amount, from); err != nil {
+                return err
+        }
+        if _, err := tx.Exec(
+                "UPDATE accounts SET balance = balance + $1 WHERE id = $2", amount, to); err != nil {
+                return err
+        }
+        return nil
 }
 
 func main() {
-	db, err := sql.Open("postgres", "postgres://{username}:{password}@{hostname}:{port}/bank?sslmode=require")
-	if err != nil {
-		log.Fatal("error connecting to the database: ", err)
-	}
+        db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+        if err != nil {
+                log.Fatal(err)
+        }
+        defer db.Close()
 
-	defer db.Close()
+        // Create the "accounts" table.
+        if _, err := db.Exec(
+                "CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY, balance INT)"); err != nil {
+                log.Fatal(err)
+        }
 
-	// Create the "accounts" table.
-	if _, err := db.Exec(
-		"CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY, balance INT)"); err != nil {
-		log.Fatal(err)
-	}
+        // Insert two rows into the "accounts" table.
+        if _, err := db.Exec(
+                "INSERT INTO accounts (id, balance) VALUES (1, 1000), (2, 250)"); err != nil {
+                log.Fatal(err)
+        }
 
-	// Insert two rows into the "accounts" table.
-	if _, err := db.Exec(
-		"INSERT INTO accounts (id, balance) VALUES (1, 1000), (2, 250)"); err != nil {
-		log.Fatal(err)
-	}
+        // Print out the balances before an account transfer (below).
+        printBalances(db)
 
-	// Print out the balances before an account transfer (below).
-	printBalances(db)
+        // Run a transfer in a transaction.
+        err = crdb.ExecuteTx(context.Background(), db, nil, func(tx *sql.Tx) error {
+                return transferFunds(tx, 1 /* from acct# */, 2 /* to acct# */, 100 /* amount */)
+        })
+        if err == nil {
+                fmt.Println("Success")
+        } else {
+                log.Fatal("error: ", err)
+        }
 
-	// Run a transfer in a transaction.
-	err = crdb.ExecuteTx(context.Background(), db, nil, func(tx *sql.Tx) error {
-		return transferFunds(tx, 1 /* from acct# */, 2 /* to acct# */, 100 /* amount */)
-	})
-	if err == nil {
-		fmt.Println("Success")
-	} else {
-		log.Fatal("error: ", err)
-	}
-
-	// Print out the balances after an account transfer.
-	printBalances(db)
+        // Print out the balances after an account transfer.
+        printBalances(db)
 }
